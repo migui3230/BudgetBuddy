7b9b1b07565310b34fba60626dced93d7e327739
Miguel Sarenas <migui0323@gmail.com>
2023-02-18 15:06:10 -0500

    Add Plaid pages, API routes and .env file for authentication and fetching bank accounts

---
+++ client/src/pages/api/auth.ts
@@ -0,0 +1,20 @@
+import { plaidClient } from "./create-link-token";
+import { NextApiRequest, NextApiResponse } from "next";
+import { AuthGetRequest } from "plaid";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const accessToken = req.body.access_token;
+    console.log("accessToken", accessToken);
+
+    const plaidResponse = await plaidClient.authGet({
+      access_token: accessToken,
+    } as AuthGetRequest);
+    res.status(200).json(plaidResponse.data);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+}
