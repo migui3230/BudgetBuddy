743d4bc4ff18313312b4cd76d79be31c372bf8ad
Miguel Sarenas <migui0323@gmail.com>
2023-03-22 21:40:18 -0400

    add clerk

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={new QueryClient()}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <ClerkProvider {...pageProps}>
+      <QueryClientProvider client={new QueryClient()}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </ClerkProvider>
   );
 }
