7997fdeb274371c972677196281e82f2b55645e4
Miguel Sarenas <migui0323@gmail.com>
2023-04-03 16:50:45 -0400

    add todo comments

--- server/app.py
+++ server/app.py
@@ -3,6 +3,12 @@ from flask import Flask, jsonify
 app = Flask(__name__)
 
 
+# TODO: interact with planetscale DB
+# TODO: do role based access control for the model
+# TODO: change access to APIs based on the role
+# TODO: once the user logs in via clerk, fetch the data for that user
+
+
 @ app.route('/')
 def index():
     data = {
