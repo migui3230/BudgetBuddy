0f560ce6b004b5c8ed6b14026f5a2ff474f2fe26
Miguel Sarenas <migui0323@gmail.com>
2023-04-03 17:22:19 -0400

    add initial interaction with database

--- server/app.py
+++ server/app.py
@@ -1,20 +1,46 @@
+import MySQLdb
+import os
 from flask import Flask, jsonify
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
+db = MySQLdb.connect(
+    host=os.getenv("HOST"),
+    user=os.getenv("USERNAME"),
+    passwd=os.getenv("PASSWORD"),
+    db=os.getenv("DATABASE"),
+    ssl_mode="VERIFY_IDENTITY",
+    ssl={
+        "ca": "/etc/ssl/cert.pem"
+    }
+)
 
-# TODO: interact with planetscale DB
+
 # TODO: do role based access control for the model
 # TODO: change access to APIs based on the role
 # TODO: once the user logs in via clerk, fetch the data for that user
 
+# TODO: create a table for users that have username, email, role
+
 
 @ app.route('/')
 def index():
-    data = {
-        'name': 'John Doe',
-    }
-    return jsonify(data)
+    cursor = db.cursor()
+    data = [
+        ('john.doe@example.com', 'johndoe'),
+        ('jane.smith@example.com', 'janesmith'),
+        ('jimmy.kim@example.com', 'jimmykim')
+    ]
+    for d in data:
+        cursor.execute(
+            "INSERT INTO users (email, username) VALUES (%s, %s)", d)
+
+    db.commit()
+    cursor.close()
+    db.close()
+    return 'data added!'
 
 
 if __name__ == '__main__':
