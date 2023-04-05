a2e7745f2e2d507616772a3d475c906475c4ffbe
Miguel Sarenas <migui0323@gmail.com>
2023-04-05 15:23:00 -0400

    add signin signup buttons and add routing to them

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -3,13 +3,23 @@ import Image from "next/image";
 import { Inter } from "@next/font/google";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { Button } from "@mantine/core";
 
 export default function Home() {
   const router = useRouter();
 
-  useEffect(() => {
-    router.push("/plaid");
-  });
+  const routeToSignIn = () => {
+    router.push("/sign-in");
+  };
 
-  return null;
+  const routeToSignUp = () => {
+    router.push("/sign-up");
+  };
+
+  return (
+    <>
+      <Button onClick={routeToSignUp}>Sign up</Button>
+      <Button onClick={routeToSignIn}>Login</Button>
+    </>
+  );
 }
