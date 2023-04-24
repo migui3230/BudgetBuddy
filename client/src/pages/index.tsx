b651a9f284718a1f41800d2d34c8d7e70f699104
ChristopherQue <89031124+ChristopherQue@users.noreply.github.com>
2023-04-24 17:03:20 -0400

    Created a login welcome page

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -28,9 +28,71 @@ export default function Home() {
   };
 
   return (
-    <>
-      <Button onClick={routeToSignUp}>Sign up</Button>
-      <Button onClick={routeToSignIn}>Login</Button>
-    </>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        backgroundColor: "#DDE2F9",
+        padding: "20px",
+        borderRadius: "10px",
+        boxShadow: "0 0 10px rgba(0,0,0,0.3)",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          width: "800px",
+          height: "500px",
+          borderRadius: "10px",
+          overflow: "hidden",
+        }}
+      >
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: "#7F92F9",
+            display: "flex",
+            padding: "40px",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            fontFamily: "'Prompt', sans-serif",
+          }}
+        >
+          <div style={{ fontSize: "32px", textAlign: "center", color: "#fff" }}>
+            Welcome to Budget Buddy!
+          </div>
+        </div>
+        <div
+          style={{
+            flex: 1,
+            backgroundColor: "#FFFFFF",
+            display: "flex",
+            flexDirection: "column",
+            padding: "40px",
+          }}
+        >
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              height: "100%",
+              flexDirection: "column",
+              justifyContent: "flex-end",
+            }}
+          >
+            <Button onClick={routeToSignUp} style={{ width: "100%" }}>
+              Sign up
+            </Button>
+            <div style={{ height: "10px" }}></div>
+            <Button onClick={routeToSignIn} style={{ width: "100%" }}>
+              Login
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
